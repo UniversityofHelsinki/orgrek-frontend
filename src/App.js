5c28bd278b2ab4507ba285218d5b6f280ee1b9d0
vmheikki <veli-matti.heikkila@helsinki.fi>
2021-07-12 09:41:46 +0300

    first api call to /api backend server

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+const ORGREK_BACKEND_SERVER = process.env.REACT_APP_ORGREK_BACKEND_SERVER;
 
 function App() {
+
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch(`${ORGREK_BACKEND_SERVER}/api`)
+        .then((res) => res.json())
+        .then((data) => setData(data.message));
+  }, []);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{!data ? "Loading..." : data}</p>
       </header>
     </div>
   );
