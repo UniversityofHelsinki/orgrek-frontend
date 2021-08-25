3539d6abe8b69fedbc0c30e092ef59447363275e
Henriikka Palva <henriikka.palva@helsinki.fi>
2021-08-25 09:44:40 +0300

    ORGREK-536 initial header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,15 @@ import { connect } from 'react-redux';
 const Header = (props) => {
     return (
         <div>
-           Helsingin yliopisto
+            <hy-site-logo color="black" size="64" url="https://www.helsinki.fi" label="Helsingin yliopisto" />
+            <hy-heading heading="h2">Organisaatiorekisteri</hy-heading>
+            <div style={{position: "relative", width: 300, height: 30}}>
+                <hy-menu-language is-mobile={false} labels='{"open":"Open", "close":"Close"}' data-menu-language='[
+                    {"langCode":"fi","abbr":"fin","label":"Finnish","url":"/fi", "isActive":"true"},
+                    {"langCode":"en","abbr":"eng","label":"English","url":"/en"},
+                    {"langCode":"sv","abbr":"swe","label":"Swedish","url":"/sv"}]'
+                />
+            </div>
         </div>
     );
 };
