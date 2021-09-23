4875c9be1bc9ae9405666796567cad75c5536ee5
Henriikka Palva <henriikka.palva@helsinki.fi>
2021-09-23 15:22:17 +0300

    OR-533 changed tree hierarchy from opetus to talous

--- src/actions/treeAction.js
+++ src/actions/treeAction.js
@@ -1,7 +1,7 @@
 export const fetchTree = () => {
     const ORGREK_BACKEND_SERVER = process.env.REACT_APP_ORGREK_BACKEND_SERVER || '';
     const PATH = '/api/tree';
-    const PARAMS = '/opetus';
+    const PARAMS = '/talous';
     return async (dispatch)  => {
         let response = await fetch(`${ORGREK_BACKEND_SERVER}${PATH}${PARAMS}`, {
             headers: {'Content-Type': 'application/json'}
