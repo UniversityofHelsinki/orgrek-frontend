5a6d97ab9ab86285e6bd9b856274ed2ca3092cdc
vmheikki <veli-matti.heikkila@helsinki.fi>
2021-09-27 15:37:11 +0300

    poc:  - put id in hidden field

--- src/components/Node.js
+++ src/components/Node.js
@@ -4,6 +4,7 @@ const Node = ({ item, selected, hasChildren, level, onToggle, onSelection }) => 
     return (
         <div style={{ paddingLeft: `${level * 25}px`}}>
             {hasChildren && <button onClick={onToggle}>{selected ? '-' : '+'}</button>}
+            <input type="hidden" value={item.id}></input>
             <span onClick={onSelection}>{item.nameFi}</span>
         </div>
     );
