ccdc2aaeadf3f1ea4aec4bacfde6e5eef2d78836
miili.halkka@office.codento.com <miili.halkka@office.codento.com>
2021-10-28 13:02:01 +0300

    Opening tree partly on default, tree tests

--- src/components/Node.js
+++ src/components/Node.js
@@ -4,7 +4,7 @@ const Node = ({ item, selected, hasChildren, level, onToggle, onSelection }) => 
     return (
         <div style={{ paddingLeft: `${level * 35}px` }}>
             <span style={{ paddingRight: '10px' }} onClick={onSelection}>{level > 0 ? item.code : ''} {item.nameFi} {item.abbreviation ? '(' + item.abbreviation + ')' : ''}</span>
-            {hasChildren && <i id={item.id} onClick={onToggle} className={selected ? 'arrow down' : 'arrow right'}></i>}
+            {hasChildren && <i data-testid={selected ? 'arrowdown' : 'arrowright'} id={item.id} onClick={onToggle} className={selected ? 'arrow down' : 'arrow right'}></i>}
         </div>
     );
 };
---
+++ tests/Tree.test.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import { screen, render } from './testUtils';
+import * as reactRedux from 'react-redux';
+import userEvent from '@testing-library/user-event';
+import Tree from '../src/components/Tree'
+
+jest.spyOn(reactRedux, 'useDispatch');
+
+jest.mock('../src/actions/treeAction', () => ({
+    __esModule: true,
+    fetchTree: jest.fn(() => { return  {
+        type: 'SUCCESS_API_GET_TREE',
+        payload: {
+            "tree": {
+                "id": 42785051,
+                "code": "HY",
+                "nameFi": "Helsingin yliopisto",
+                "nameEn": "University of Helsinki",
+                "nameSv": "Helsingfors universitet",
+                "displayNameFi": "Helsingin yliopisto (HY)",
+                "displayNameEn": "University of Helsinki (HY)",
+                "displayNameSv": "Helsingfors universitet (HY)",
+                "type": "yritys_yhteiso",
+                "abbreviation": "HY",
+                "children": [
+                {
+                    "id": 33539259,
+                    "code": "KOULOHJ",
+                    "nameFi": "Koulutusohjelmat",
+                    "nameEn": "Degree Programmes",
+                    "nameSv": "Utbildningsprogram",
+                    "displayNameFi": "HY, Koulutusohjelmat (KOULOHJ)",
+                    "displayNameEn": "HY, Degree Programmes (KOULOHJ)",
+                    "displayNameSv": "HY, Utbildningsprogram (KOULOHJ)",
+                    "type": "koontiyksikko",
+                    "abbreviation": "KOULOHJ",
+                    "parentAbbreviation": "HY",
+                    "children": [
+                    {
+                        "id": 54806742,
+                        "code": "H92",
+                        "nameFi": "Tohtoriohjelmat",
+                        "nameEn": "Doctoral Programmes",
+                        "nameSv": "Doktorandprogram",
+                        "displayNameFi": "HY, Tohtoriohjelmat (TRI)",
+                        "displayNameEn": "HY, Doctoral Programmes (TRI)",
+                        "displayNameSv": "HY, Doktorandprogram (TRI)",
+                        "type": "tiedekunnan kaltainen,koontiyksikko",
+                        "abbreviation": "TRI",
+                        "parentAbbreviation": "HY",
+                        "children": [
+                        {
+                            "id": 61974091,
+                            "code": "H920",
+                            "nameFi": "Humanistis-yhteiskuntatieteellinen tutkijakoulu",
+                            "nameEn": "Doctoral School in the Humanities and Social Sciences",
+                            "nameSv": "Humanistisk-samhällsvetenskapliga forskarskolan",
+                            "displayNameFi": "HY-TRI, Humanistis-yhteiskuntatieteellinen tutkijakoulu (HYMY)",
+                            "displayNameEn": "HY-TRI, Doctoral School in the Humanities and Social Sciences (HYMY)",
+                            "displayNameSv": "HY-TRI, Humanistisk-samhällsvetenskapliga forskarskolan (HYMY)",
+                            "type": "tutkijakoulu",
+                            "abbreviation": "HYMY",
+                            "parentAbbreviation": "HY-TRI"
+                        }]
+                    }]
+                }]
+            },
+        }
+    }})
+}));
+
+test('Tree renders', () => {    
+    render(<Tree />);
+    const tree = screen.getByTestId("tree");
+    expect(tree).toBeDefined();
+});
+
+test('There are two levels visible on default', () => {
+    render(<Tree />);    
+    expect(screen.getByText('Helsingin yliopisto (HY)')).toBeInTheDocument;    
+    expect(screen.getByText('KOULOHJ Koulutusohjelmat (KOULOHJ)')).toBeInTheDocument;    expect(() => screen.getByText('H920 Humanistis-yhteiskuntatieteellinen tutkijakoulu (HYMY)')).toThrow('Unable to find an element'); 
+    expect(() => screen.getByText('H920 Humanistis-yhteiskuntatieteellinen tutkijakoulu (HYMY)')).toThrow('Unable to find an element'); 
+    expect(() => screen.getByText('H92 Tohtoriohjelmat (TRI)')).toThrow('Unable to find an element'); 
+
+})
+
+test('Opening and closing tree levels', () => {
+    render(<Tree />);     
+    expect(() => screen.getByText('H920 Humanistis-yhteiskuntatieteellinen tutkijakoulu (HYMY)')).toThrow('Unable to find an element'); 
+    let input = screen.getByTestId('arrowright');
+    userEvent.click(input);
+    input = screen.getByTestId('arrowright');
+    userEvent.click(input);
+    expect(() => screen.getByTestId('arrowright')).toThrow('Unable to find an element');    
+    expect(screen.getByText('Helsingin yliopisto (HY)')).toBeInTheDocument;    
+    expect(screen.getByText('KOULOHJ Koulutusohjelmat (KOULOHJ)')).toBeInTheDocument;        
+    expect(screen.getByText('H920 Humanistis-yhteiskuntatieteellinen tutkijakoulu (HYMY)')).toBeInTheDocument;    
+    expect(screen.getByText('H92 Tohtoriohjelmat (TRI)')).toBeInTheDocument;
+    input = screen.getAllByTestId('arrowdown');
+    userEvent.click(input[0]);
+    expect(() => screen.getByText('KOULOHJ Koulutusohjelmat (KOULOHJ)')).toThrow('Unable to find an element'); 
+});
