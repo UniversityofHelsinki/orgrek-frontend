fcf7682014c4966ae3b78f56e50c3f5dd51dc889
miili.halkka@office.codento.com <miili.halkka@office.codento.com>
2021-11-05 10:20:07 +0200

    setup

--- src/components/Node.js
+++ src/components/Node.js
@@ -6,7 +6,7 @@ import { fetchNode, fetchNodeAttributes } from '../actions/nodeAction';
 const Node = (props) => {
     return (
         <div style={{ paddingLeft: `${props.level * 35}px` }}>
-            <span style={{ paddingRight: '10px' }} onClick={() => props.onNodeSelection()}>{props.level > 0 ? props.item.code : ''} {props.item.nameFi} {props.item.abbreviation ? '(' + props.item.abbreviation + ')' : ''}</span>
+            <span style={{ paddingRight: '10px' }} onClick={() => props.onNodeSelection()}>{props.level > 0 ? props.item.code : ''} {props.item.nameFi} {props.item.abbreviation ? '(' + props.item.abbreviation + ')' : ''} {props.item.id}</span>
             {props.hasChildren && <i data-testid={props.selected ? 'arrowdown' : 'arrowright'}  id={props.item.id} onClick={props.onToggle} className={props.selected ? 'arrow down' : 'arrow right'}></i>}
         </div>
     );
@@ -14,8 +14,9 @@ const Node = (props) => {
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
     onNodeSelection: () => {
-        dispatch(fetchNode(ownProps.item.id));
-        dispatch(fetchNodeAttributes(ownProps.item.id));
+        console.log(ownProps.item);
+        //dispatch(fetchNode(ownProps.item.id));
+        //dispatch(fetchNodeAttributes(ownProps.item.id));
         dispatch(fetchNodeParents(ownProps.item.id));
         dispatch(fetchNodeChildren(ownProps.item.id));
     }
