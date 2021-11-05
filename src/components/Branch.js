4c1c445fbf0f21e838e6b123aed6bdc1ced7dd62
miili.halkka@office.codento.com <miili.halkka@office.codento.com>
2021-11-05 12:25:59 +0200

    wip

--- src/components/Branch.js
+++ src/components/Branch.js
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
 import Node from './Node';
 
-const Branch = ({ item, level }) => {
+const Branch = (props, { item, level }) => {
+    console.log(item);
     const [selected, setSelected] = useState(level >= 1 ? false : true); // open tree on load
     const hasChildren = item.children && item.children.length !== 0;
 
     const renderBranches = () => {
         if (hasChildren) {
             const newLevel = level + 1;
-            console.log(item);
-            item.children.sort((prev, next) => prev.id - next.id);
-            console.log(item.children);
+            const sortedChildren = sortChildren(item.children, props.order_by);
             return item.children.map((child) => {
                 return <Branch key={child.id} item={child} level={newLevel} />;
             });
@@ -43,4 +43,28 @@ const Branch = ({ item, level }) => {
     );
 };
 
-export default Branch;
+function sortChildren (children, attributeToSortBy) {
+    let childrenWithAttr = [];
+    let childrenRest = [];
+    children.map((item) => {
+        const attributes = []; //node attributes
+        const attribute = attributes.find(elem => elem.key === attributeToSortBy);
+        item.sortValue = attribute;
+        if (attribute !== undefined) {
+            childrenWithAttr.push(item);
+        } else {
+            childrenRest.push(item);
+        }
+    });
+
+    childrenWithAttr.sort((prev, next) => prev.sortValue - next.sortValue);
+    childrenRest.sort((prev, next) => prev.nameFi - next.nameFi);
+    console.log(children);
+    return childrenWithAttr + childrenRest;
+}
+
+const mapStateToProps = state => ({
+    order_by : state.tree.order_by
+});
+
+export default connect(mapStateToProps)(Branch);
