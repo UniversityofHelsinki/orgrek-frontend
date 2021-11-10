a2499a5255520f01120319411ae646a3764bc8ce
vmheikki <veli-matti.heikkila@helsinki.fi>
2021-11-10 15:08:36 +0200

    removed i18n null check because it is used everywhere

--- src/actions/utilAction.js
+++ src/actions/utilAction.js
@@ -3,14 +3,14 @@ export const showValidity = (startDate, endDate, i18n) => {
         return new Date(startDate).toLocaleDateString('fi-FI') + ' – ' + new Date(endDate).toLocaleDateString('fi-FI');
     }
     if (startDate) {
-        return i18n ? i18n.t('from_date') : '' + new Date(startDate).toLocaleDateString('fi-FI');
+        return  i18n.t('from_date') + new Date(startDate).toLocaleDateString('fi-FI');
     }
 
     if (endDate) {
-        return i18n ? i18n.t('until_date') : ''  +  new Date(endDate).toLocaleDateString('fi-FI');
+        return i18n.t('until_date') +  new Date(endDate).toLocaleDateString('fi-FI');
     }
 
-    return i18n ? i18n.t('not_specified') : '';
+    return i18n.t('not_specified');
 };
 
 export default showValidity;
