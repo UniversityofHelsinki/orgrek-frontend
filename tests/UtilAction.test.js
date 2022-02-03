4c0ad2405bbbe7c23ebec3dada181814da3d42f3
miili.halkka@office.codento.com <miili.halkka@office.codento.com>
2022-02-03 08:19:49 +0200

    Changing test comparison to regex to accept freedom dates too

--- tests/UtilAction.test.js
+++ tests/UtilAction.test.js
@@ -148,21 +148,21 @@ test('the from dates render as expected', () => {
   const t = (str) => str
   const i18n = {language: 'fi'};
   const parseDate = showValidity('2013-12-31T20:00:00.000+00:00', null, i18n, t);
-  expect(parseDate).toMatch(/31[./]12[./]2013from_date/);
+  expect(parseDate).toMatch(/12|31[./]12|31[./]2013from_date/);
 })
 
 test('the until dates render as expected', () => {
   const t = (str) => str
   const i18n = {language: 'fi'};
   const parseDate = showValidity(null, '2016-12-31 00:00:00', i18n, t);
-  expect(parseDate).toMatch(/31[./]12[./]2016until_date/);
+  expect(parseDate).toMatch(/12|31[./]12|31[./]2016until_date/);
 })
 
 test('the time period dates render as expected', () => {
   const t = (str) => str
   const i18n = {language: 'fi'};
   const parseDate = showValidity('2013-12-31T20:00:00.000+00:00', '2016-12-31 00:00:00', i18n, t);
-  expect(parseDate).toMatch(/31[./]12[./]2013 - 31[./]12[./]2016/);
+  expect(parseDate).toMatch(/12|31[./]12|31[./]2013 - 12|31[./]12|31[./]2016/);
 })
 
 test("the missing date message rendes as expected", () => {
