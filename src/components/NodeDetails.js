ed3f12a4fcc4c98cbf136b94b4ee86fde1b44376
vmheikki <veli-matti.heikkila@helsinki.fi>
2022-06-02 14:06:24 +0300

    sort name attributes by date

--- src/components/NodeDetails.js
+++ src/components/NodeDetails.js
@@ -32,10 +32,30 @@ const NodeDetails = (props) => {
         return result;
     };
 
+    const sortNameAttributesByDate = (elems, order) => {
+
+        let result = [];
+
+        for (let property in order) {
+
+            const filteredBatch = elems.filter(e => {
+              return e.key === property;
+            });
+
+            filteredBatch.sort((a,b) => {
+                return new Date(b.endDate) - new Date(a.startDate);
+            });
+
+            result.push(filteredBatch);
+        }
+
+        return result.flat();
+    };
+
     const orderNameAttributesByLanguage = (elems) => {
         const order = { name_fi : 0, name_sv : 1, name_en : 2, default: 3 };
         elems.sort((a,b) => order[a.key] - order[b.key]);
-        return elems;
+        return sortNameAttributesByDate(elems, order);
     };
 
     const isLanguageAttribute = (elem) => {
