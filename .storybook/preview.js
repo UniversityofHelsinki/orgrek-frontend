a13b57325f38feb8033f301a3c1b681fc26e2604
Ville Vornanen <2314-vornavil@users.noreply.version.helsinki.fi>
2023-02-10 23:20:59 +0200

    feat(storybook): configure theme provider

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,7 @@
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { DocsContainer } from '@storybook/addon-docs';
+import theme from '../src/theme';
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
@@ -6,4 +10,21 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  docs: {
+    container: (props) => (
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <DocsContainer {...props} />
+      </ThemeProvider>
+    ),
+  },
 };
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Story />
+    </ThemeProvider>
+  ),
+];
