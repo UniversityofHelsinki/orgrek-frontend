89b896fb654f864af4708b7cc67dc4ee1088af5d
Ville Vornanen <2314-vornavil@users.noreply.version.helsinki.fi>
2023-02-15 13:33:40 +0200

    refactor(tests): mock react-i18next globally in all tests
    Moved the mock to setupTests.js instead of mocking it in all tests
    separately.

--- tests/Tree.test.js
+++ tests/Tree.test.js
@@ -88,19 +88,6 @@ jest.mock('../src/reducers/treeReducer', () => {
   };
 });
 
-jest.mock('react-i18next', () => ({
-  // this mock makes sure any components using the translate hook can use it without a warning being shown
-  useTranslation: () => {
-    return {
-      t: (str) => str,
-      i18n: {
-        changeLanguage: () => new Promise(() => {}),
-        language: 'fi',
-      },
-    };
-  },
-}));
-
 test('Tree renders', () => {
   render(<Tree />);
   const tree = screen.getByTestId('tree');
