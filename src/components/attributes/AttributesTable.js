3e85438588d484bfabc27a42e34dd9fffe2ce5a8
Ville Vornanen <2314-vornavil@users.noreply.version.helsinki.fi>
2023-02-17 20:34:34 +0200

    feat(components): add AttributesTable

---
+++ src/components/attributes/AttributesTable.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+import TableBody from '@mui/material/TableBody';
+import TableContainer from '@mui/material/TableContainer';
+import Validity from './Validity';
+import { useTranslation } from 'react-i18next';
+
+const AttributesTable = ({ columns, data, summary, keyFn }) => {
+  const { t } = useTranslation();
+
+  const defaultColumns = [
+    { label: t('text_language_header'), render: (item) => t(item.key) },
+    { label: t('name'), render: (item) => t(item.value) },
+    {
+      label: t('valid_dates'),
+      render: (item) => (
+        <Validity startDate={item.startDate} endDate={item.endDate} />
+      ),
+    },
+  ];
+  const defaultKeyFn = (item) =>
+    `${item.key}-${item.startDate}-${item.endDate}`;
+
+  const cols = columns || defaultColumns;
+  const getKey = keyFn || defaultKeyFn;
+
+  const renderedHeaders = cols.map((column) => (
+    <TableCell key={column.label}>{column.label}</TableCell>
+  ));
+
+  const renderedRows = data.map((item) => {
+    const renderedCells = cols.map((column) => (
+      <TableCell key={column.label}>{column.render(item)}</TableCell>
+    ));
+
+    return <TableRow key={getKey(item)}>{renderedCells}</TableRow>;
+  });
+
+  return (
+    <TableContainer component="div">
+      <Table size="small" aria-label={summary} summary={summary}>
+        <TableHead>
+          <TableRow>{renderedHeaders}</TableRow>
+        </TableHead>
+        <TableBody>{renderedRows}</TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default AttributesTable;
---
+++ src/stories/components/attributes/AttributesTable.stories.js
@@ -0,0 +1,37 @@
+import AttributesTable from '../../../components/attributes/AttributesTable';
+
+export default {
+  component: AttributesTable,
+};
+
+export const Default = {
+  args: {
+    summary: 'Nimitiedot',
+    data: [
+      {
+        id: 4821,
+        nodeId: '4820',
+        key: 'name_en',
+        value: 'IT Solutions',
+        startDate: null,
+        endDate: null,
+      },
+      {
+        id: 4822,
+        nodeId: '4820',
+        key: 'name_sv',
+        value: 'Datatekniklösningar',
+        startDate: null,
+        endDate: null,
+      },
+      {
+        id: 4826,
+        nodeId: '4820',
+        key: 'name_fi',
+        value: 'Tietotekniikkaratkaisut',
+        startDate: null,
+        endDate: null,
+      },
+    ],
+  },
+};
