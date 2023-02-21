57ffde9468e0ac7e84ed7d698cb2cc23d0e013f3
Ville Vornanen <2314-vornavil@users.noreply.version.helsinki.fi>
2023-02-21 13:07:11 +0200

    feat(mockStore): add legacy tree reducer

--- src/mockStore/mockTree.js
+++ src/mockStore/mockTree.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import legacyReducer from '../reducers/treeReducer';
 
 const initialState = {
   tree: {},
@@ -19,6 +20,9 @@ const initialState = {
 const treeSlice = createSlice({
   name: 'tree',
   initialState,
+  extraReducers(builder) {
+    builder.addDefaultCase(legacyReducer);
+  },
 });
 
 export const treeReducer = treeSlice.reducer;
