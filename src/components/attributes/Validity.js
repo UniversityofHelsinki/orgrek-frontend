3a97e5d0c239e5698ee9ec4e25aa1d6c05476016
Ville Vornanen <2314-vornavil@users.noreply.version.helsinki.fi>
2023-02-24 10:09:02 +0200

    fix(Validity): add missing typography

--- src/components/attributes/Validity.js
+++ src/components/attributes/Validity.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { showValidity } from '../../actions/utilAction';
+import Typography from '@mui/material/Typography';
 
 const Validity = ({ startDate, endDate }) => {
   const { t, i18n } = useTranslation();
 
   return (
-    <span data-testid="validity">
+    <Typography component="span" variant="body1" data-testid="validity">
       {showValidity(startDate, endDate, i18n, t)}
-    </span>
+    </Typography>
   );
 };
 
