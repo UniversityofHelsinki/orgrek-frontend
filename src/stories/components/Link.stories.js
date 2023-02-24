0d1dcc99e528b19c1cc82e67c05a14fbfa1538d9
Ville Vornanen <2314-vornavil@users.noreply.version.helsinki.fi>
2023-02-24 18:20:23 +0200

    feat(component): add Link

---
+++ src/stories/components/Link.stories.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Link from '../../components/Link';
+
+export default {
+  component: Link,
+};
+
+export const NodeLink = {
+  args: {
+    to: '',
+    node: 12345678,
+    hierarchies: '',
+    text: 'Helsingin yliopisto (HY)',
+  },
+  render: ({ text, ...args }) => <Link {...args}>{text}</Link>,
+};
+
+export const HierarchiesLink = {
+  ...NodeLink,
+  args: {
+    to: '',
+    node: '',
+    hierarchies: 'talous,opetus',
+    text: 'Hierarchy link',
+  },
+};
+
+export const PathLink = {
+  ...NodeLink,
+  args: {
+    to: '/texts',
+    node: '',
+    hierarchies: '',
+    text: 'Texts',
+  },
+};
