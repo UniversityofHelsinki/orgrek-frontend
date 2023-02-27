2c4fb3028c6bd8b96da4f100184b5907cabfe1b3
Ville Vornanen <2314-vornavil@users.noreply.version.helsinki.fi>
2023-02-27 13:16:22 +0200

    fix(Layout): remove unnecessary styles

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,7 +7,7 @@ import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
-    <div className="App">
+    <div>
       <SkipNavLink id="main-content" />
       <Header2 />
       <Header />
