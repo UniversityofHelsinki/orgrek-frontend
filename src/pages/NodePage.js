17dd8056717e9aee6a6f21e20390029b1150b26b
Emmi Sulander <emmi.sulander@helsinki.fi>
2023-03-03 10:39:23 +0200

    Wrap NodePade with Container

--- src/pages/NodePage.js
+++ src/pages/NodePage.js
@@ -3,6 +3,7 @@ import NodeDetails2 from '../components/nodeDetails/NodeDetails2';
 import NodeDetails from '../components/NodeDetails';
 import React from 'react';
 import { useSelector } from 'react-redux';
+import Container from '@mui/material/Container';
 import Grid from '@mui/material/Unstable_Grid2';
 import useSelectedHierarchies from '../hooks/useSelectedHierarchies';
 
@@ -13,7 +14,7 @@ const NodePage = () => {
   const editMode = useSelector((state) => state.editModeReducer.edit);
 
   return (
-    <>
+    <Container>
       <Grid container>
         <Grid xs={12} md={5} lg={4}>
           <Hierarchy />
@@ -23,7 +24,7 @@ const NodePage = () => {
           {editMode && <NodeDetails />}
         </Grid>
       </Grid>
-    </>
+    </Container>
   );
 };
 
