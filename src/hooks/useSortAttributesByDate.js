083b560ab4828bf4b9c6e9e26cd92cafe9458586
Ville Vornanen <2314-vornavil@users.noreply.version.helsinki.fi>
2023-03-07 12:15:51 +0200

    fix(useSortAttributesByDate): make copy instead of sorting the original array

--- src/hooks/useSortAttributesByDate.js
+++ src/hooks/useSortAttributesByDate.js
@@ -1,6 +1,7 @@
 const useSortAttributesByDate = (elems) => {
-  let result = [];
-  elems.sort((a, b) => {
+  let result = [...elems];
+
+  result.sort((a, b) => {
     return (
       (!(a.endDate || b.endDate) && 0) ||
       (!a.endDate && -1) ||
@@ -8,8 +9,8 @@ const useSortAttributesByDate = (elems) => {
       new Date(b.endDate) - new Date(a.endDate)
     );
   });
-  result.push(elems);
-  return result.flat();
+
+  return result;
 };
 
 export default useSortAttributesByDate;
