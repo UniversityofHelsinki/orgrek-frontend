734b5835288224fc6f24d1da64478884c30f75e9
Ville Vornanen <2314-vornavil@users.noreply.version.helsinki.fi>
2023-03-10 18:26:12 +0200

    feat(hooks): add useFilterAttributesByDate

--- src/components/nodeDetails/NameSection.js
+++ src/components/nodeDetails/NameSection.js
@@ -12,21 +12,48 @@ import {
   useSaveNameAttributesMutation,
 } from '../../store';
 import useSortAttributesByDate from '../../hooks/useSortAttributesByDate';
+import useFilterAttributesByDate from '../../hooks/useFilterAttributesByDate';
+
+const toFormValues = (data) => {
+  const nameFi = data.filter((value) => value.key === 'name_fi');
+  const nameSv = data.filter((value) => value.key === 'name_sv');
+  const nameEn = data.filter((value) => value.key === 'name_en');
+
+  return { nameFi, nameSv, nameEn };
+};
 
 const NameSection = () => {
   const { t } = useTranslation();
   const nodeId = useNodeId();
   const { data, error, isFetching } = useGetNameAttributesQuery(nodeId);
   const [saveNameAttributes, saveResult] = useSaveNameAttributesMutation();
+
+  // In edit mode data includes also history and future
   const sortedData = useSortAttributesByDate(data);
+
+  // In view mode filter history and future depending on selection
+  const sortedAndFilteredData = useFilterAttributesByDate(data);
 
   // TODO: show success snackbar when saveResult is completed
   // success message t('update_attributes_success')
+
   // TODO: show error snackbar if saveResult has error
 
   if (error) {
     // TODO: Fetching data failed, show error snackbar
   }
+
+  // Validates form values every time when the values change
+  // Submit button is disabled when errors contain any truthy values
+  // EditableContent handles displaying form-level validation error messages
+  const validate = (values) => {
+    const errors = {};
+
+    // TODO: add validation rules here
+    // errors.error = t('…');
+
+    return errors;
+  };
 
   const handleSubmit = (values) => {
     // TODO: call and return saveNameAttributes({ nodeId, values }).unwrap()
@@ -44,11 +71,10 @@ const NameSection = () => {
   ];
 
   const title = t('name_info');
-  const empty = sortedData.length === 0;
+  const empty = sortedAndFilteredData.length === 0;
 
-  const nameFi = sortedData.filter((value) => value.key === 'name_fi');
-  const nameSv = sortedData.filter((value) => value.key === 'name_sv');
-  const nameEn = sortedData.filter((value) => value.key === 'name_en');
+  // Sort by language
+  const { nameFi, nameSv, nameEn } = toFormValues(sortedAndFilteredData);
 
   const renderedContent = (
     <AttributesTable
@@ -66,7 +92,8 @@ const NameSection = () => {
     >
       <EditableContent
         editorComponent={<EditNameForm />}
-        initialValues={{ nameFi, nameSv, nameEn }}
+        validate={validate}
+        initialValues={toFormValues(sortedData)}
         onSubmit={handleSubmit}
       >
         <Placeholder empty={empty} placeholder={t('nameInfo.empty')}>
