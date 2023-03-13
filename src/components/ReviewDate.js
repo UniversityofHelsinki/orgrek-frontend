e9cb0262a3ef4df7d7afb06d6a3a881fe24c3605
Emmi Sulander <emmi.sulander@helsinki.fi>
2023-03-13 11:24:19 +0200

    Remove extra toLocaleDateString from ReviewDate

--- src/components/ReviewDate.js
+++ src/components/ReviewDate.js
@@ -34,9 +34,7 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = (dispatch) => ({
   onDayChange: (date) => {
-    isValid(date)
-      ? dispatch(changeDate(new Date(date).toLocaleDateString('fi-FI')))
-      : '';
+    isValid(date) ? dispatch(changeDate(new Date(date))) : '';
   },
 });
 
