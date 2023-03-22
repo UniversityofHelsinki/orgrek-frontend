334c3e531292a29ded637e2216c9d469eaded621
Ville Vornanen <2314-vornavil@users.noreply.version.helsinki.fi>
2023-03-22 15:36:39 +0200

    test(storybook): configure test-runner for a11y tests

---
+++ .storybook/test-runner.js
@@ -0,0 +1,18 @@
+const { injectAxe, checkA11y } = require('axe-playwright');
+
+module.exports = {
+  async preRender(page) {
+    await injectAxe(page);
+  },
+  async postRender(page) {
+    await checkA11y(
+      page,
+      '#root',
+      {
+        detailedReport: true,
+      },
+      false,
+      'v2'
+    );
+  },
+};
