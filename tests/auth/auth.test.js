02441f96cbac7a21314bf1e18cbd48edd128117a
Ville Vornanen <2314-vornavil@users.noreply.version.helsinki.fi>
2023-03-27 16:50:57 +0300

    test(auth): add more tests

--- tests/auth/auth.test.js
+++ tests/auth/auth.test.js
@@ -1,4 +1,8 @@
-import { isAuthorized } from '../../src/auth';
+import { isAdmin, isAuthorized } from '../../src/auth';
+
+global.console = {
+  error: jest.fn(),
+};
 
 test.each([
   { roles: [], action: { allowRoles: ['ROLE_READER'] }, expected: false },
@@ -32,7 +36,40 @@ test.each([
     action: { allowRoles: ['ROLE_READER', 'ROLE_ADMIN'] },
     expected: true,
   },
-])('is authorized %o', ({ roles, action, expected }) => {
+])('isAuthorized %o', ({ roles, action, expected }) => {
   const user = { roles };
   expect(isAuthorized(user, action)).toBe(expected);
+});
+
+test('isAuthorized throws error if action is string', () => {
+  const user = { roles: [] };
+  expect(() => isAuthorized(user, 'invalid action')).toThrowError(
+    'Expected action as object'
+  );
+});
+
+test('isAuthorized throws error if allowRoles is undefined', () => {
+  const user = { roles: [] };
+  expect(() => isAuthorized(user, { allow: true })).toThrowError(
+    'Action is missing allowRoles'
+  );
+});
+
+test.each([
+  { roles: [], expected: false },
+  {
+    roles: ['ROLE_READER'],
+    expected: false,
+  },
+  {
+    roles: ['ROLE_ADMIN'],
+    expected: true,
+  },
+  {
+    roles: ['ROLE_READER', 'ROLE_ADMIN'],
+    expected: true,
+  },
+])('isAdmin %o', ({ roles, expected }) => {
+  const user = { roles };
+  expect(isAdmin(user)).toBe(expected);
 });
