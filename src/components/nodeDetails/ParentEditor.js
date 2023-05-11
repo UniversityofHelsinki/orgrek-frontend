01e5cbb9cd0a2caa933c6987fc505c73cb59dc05
Riku Hänninen <riku.hanninen@helsinki.fi>
2023-05-11 14:21:24 +0300

    OR-1049 -- Uuden yksikön lisääminen - hakulaatikko.

--- src/components/nodeDetails/ParentEditor.js
+++ src/components/nodeDetails/ParentEditor.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import TextField from '@mui/material/TextField';
 import { MenuItem } from '@mui/material';
@@ -7,14 +7,15 @@ import useForm from '../../hooks/useForm';
 import AttributeEditor from '../attributes/AttributeEditor';
 import { useParents } from '../../hooks/useParents';
 import useContentLanguage from '../../hooks/useContentLanguage';
+import NodeField from '../inputs/NodeField';
+import Stack from '@mui/material/Stack';
+import { Typography, Box } from '@mui/material';
 
-const ParentEditor = () => {
+const ParentEditor = ({ parents, onParentChange }) => {
   const { t } = useTranslation();
-  const { parents } = useParents();
-  const contentLanguage = useContentLanguage();
-  const data = parents[contentLanguage] || [];
+  const [parentNames, setParentNames] = useState(parents);
   const getParentName = (id) => {
-    return data.find((parent) => `s${parent.uniqueId}` === id).fullName;
+    return parentNames.find((parent) => `s${parent.uniqueId}` === id)?.fullName;
   };
 
   const selectableHierarchies = useSelector((s) => {
@@ -36,24 +37,58 @@ const ParentEditor = () => {
 
   const getDisplayText = (value) => t(value.value);
 
-  return Object.entries(values).map(([parentId, hierarchies], i) => (
-    <AttributeEditor
-      overlap
-      renderValueField={renderValueField}
-      getDisplayText={getDisplayText}
-      key={`${parentId}-${i}`}
-      path={`${parentId}`}
-      attributeLabel={getParentName(parentId)}
-      attributeKey={parentId}
-      data={hierarchies}
-      onChange={(newData) => {
-        setValues({
-          ...values,
-          [parentId]: newData,
-        });
-      }}
-    />
-  ));
+  const nodeIsAlreadyParent = (node) => {
+    return values[`s${node.id}`];
+  };
+
+  const addParent = (event, node, reason) => {
+    if (!node || nodeIsAlreadyParent(node)) {
+      return;
+    }
+    const newValues = {
+      ...values,
+      [`s${node.id}`]: [],
+    };
+    setParentNames([
+      ...parentNames,
+      { uniqueId: node.id, fullName: node.name },
+    ]);
+    setValues(newValues);
+    onParentChange(newValues);
+  };
+
+  return (
+    <Stack>
+      <Box mb={2}>
+        <Typography component="p" variant="h6" mb={2}>
+          {t('new_upper_unit')}
+        </Typography>
+        <NodeField
+          style={{ width: '50%' }}
+          variant="search"
+          onChange={addParent}
+        />
+      </Box>
+      {Object.entries(values).map(([parentId, hierarchies], i) => (
+        <AttributeEditor
+          overlap
+          renderValueField={renderValueField}
+          getDisplayText={getDisplayText}
+          key={`${parentId}-${i}`}
+          path={`${parentId}`}
+          attributeLabel={getParentName(parentId) || parentId}
+          attributeKey={parentId}
+          data={hierarchies}
+          onChange={(newData) => {
+            setValues({
+              ...values,
+              [parentId]: newData,
+            });
+          }}
+        />
+      ))}
+    </Stack>
+  );
 };
 
 export default ParentEditor;
