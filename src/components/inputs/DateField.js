84f30c7954560a5be604deeaceab80ffae5d309f
Ville Vornanen <2314-vornavil@users.noreply.version.helsinki.fi>
2023-06-11 23:29:48 +0300

    feat(DateField): add renderInput prop

--- src/components/inputs/DateField.js
+++ src/components/inputs/DateField.js
@@ -11,34 +11,39 @@ const DateField = ({
   fullWidth,
   onChange,
   variant,
+  renderInput,
   ...props
-}) => (
-  <DatePicker
-    disableMaskedInput
-    onChange={(date, keyboardInputValue) => {
-      // Date picker returns Date in user's local time zone
-      // Convert value to ISO date string, ensuring it is always formatted
-      // in the correct time zone
-      onChange && onChange(toISODateStringLocal(date), keyboardInputValue);
-    }}
-    {...props}
-    renderInput={(textFieldProps) => (
-      <TextField
-        {...textFieldProps}
-        helperText={helperText}
-        size={size}
-        required={required}
-        error={error}
-        fullWidth={fullWidth}
-        variant={variant}
-        sx={{
-          '& .MuiInputAdornment-root .MuiButtonBase-root': {
-            marginRight: variant === 'standard' ? -1 : -1.5,
-          },
-        }}
-      />
-    )}
-  />
-);
+}) => {
+  const defaultRenderInput = (textFieldProps) => (
+    <TextField
+      {...textFieldProps}
+      helperText={helperText}
+      size={size}
+      required={required}
+      error={error}
+      fullWidth={fullWidth}
+      variant={variant}
+      sx={{
+        '& .MuiInputAdornment-root .MuiButtonBase-root': {
+          marginRight: variant === 'standard' ? -1 : -1.5,
+        },
+      }}
+    />
+  );
+
+  return (
+    <DatePicker
+      disableMaskedInput
+      onChange={(date, keyboardInputValue) => {
+        // Date picker returns Date in user's local time zone
+        // Convert value to ISO date string, ensuring it is always formatted
+        // in the correct time zone
+        onChange && onChange(toISODateStringLocal(date), keyboardInputValue);
+      }}
+      {...props}
+      renderInput={renderInput || defaultRenderInput}
+    />
+  );
+};
 
 export default DateField;
