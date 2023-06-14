c81bec5a582ff6bc5eb6a9aa62b33dcbd002eae6
Ville Vornanen <2314-vornavil@users.noreply.version.helsinki.fi>
2023-06-14 17:10:26 +0300

    fix(storybook): fix LoadingOverlay example

--- src/stories/components/LoadingOverlay.stories.js
+++ src/stories/components/LoadingOverlay.stories.js
@@ -24,11 +24,13 @@ export const LoadingOverlay = {
           fermentum vestibulum. Aenean non bibendum neque. Aliquam venenatis
           dolor quis laoreet varius. Nulla sit amet felis justo.
         </Typography>
-        <Box
-          sx={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}
-        >
-          {loading && <LoadingOverlayComponent />}
-        </Box>
+        {loading && (
+          <Box
+            sx={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}
+          >
+            <LoadingOverlayComponent />
+          </Box>
+        )}
       </Box>
     );
   },
