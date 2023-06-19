9dd8adcf59250a6e68e449247bcb418727cbb4b5
Riku Hänninen <riku.hanninen@helsinki.fi>
2023-06-19 07:27:16 +0300

    Lisää labelit storybookkiin.

--- src/stories/components/Switch.stories.js
+++ src/stories/components/Switch.stories.js
@@ -19,10 +19,21 @@ export const MediumSwitch = {
   render: (args) => {
     return (
       <>
-        <Switch {...args} />
-        <Switch {...args} checked={true} />
-        <Switch {...args} disabled={true} />
-        <Switch {...args} checked={true} disabled={true} />
+        <label htmlFor="medium">Switch</label>
+        <Switch {...args} id="medium" />
+        <label htmlFor="medium-checked">Checked Switch</label>
+        <Switch {...args} checked={true} id="medium-checked" />
+        <label htmlFor="medium-disabled">Disabled Switch</label>
+        <Switch {...args} disabled={true} id="medium-disabled" />
+        <label htmlFor="medium-checked-disabled">
+          Checked and disabled Switch
+        </label>
+        <Switch
+          {...args}
+          checked={true}
+          disabled={true}
+          id="medium-checked-disabled"
+        />
       </>
     );
   },
@@ -35,10 +46,21 @@ export const SmallSwitch = {
   render: (args) => {
     return (
       <>
-        <Switch {...args} />
-        <Switch {...args} checked={true} />
-        <Switch {...args} disabled={true} />
-        <Switch {...args} checked={true} disabled={true} />
+        <label htmlFor="small">Small Switch</label>
+        <Switch {...args} id="small" />
+        <label htmlFor="small-checked">Checked small switch</label>
+        <Switch {...args} checked={true} id="small-checked" />
+        <label htmlFor="small-disabled">Disabled switch</label>
+        <Switch {...args} disabled={true} id="small-disabled" />
+        <label htmlFor="small-checked-disabled">
+          Checked and disabled switch
+        </label>
+        <Switch
+          {...args}
+          checked={true}
+          disabled={true}
+          id="small-checked-disabled"
+        />
       </>
     );
   },
