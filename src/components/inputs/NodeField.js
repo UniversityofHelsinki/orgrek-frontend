e8261fadcb109fa2958fd25d2a93ba748036d560
Emmi Sulander <emmi.sulander@helsinki.fi>
2023-08-07 12:04:21 +0300

    Change label in search field for the longer version

--- src/components/inputs/NodeField.js
+++ src/components/inputs/NodeField.js
@@ -115,7 +115,7 @@ const NodeField = ({
               ...InputProps,
               endAdornment: InputProps.endAdornment || inputAdornment,
             }}
-            label={t('search')}
+            label={t('search_by_name_or_code')}
             placeholder={placeholder || t('type_three_char_to_start_search')}
             helperText={helperText}
             required={required}
