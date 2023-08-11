46eddbd475b11503475682363010ce7db0ec3c07
Emmi Sulander <emmi.sulander@helsinki.fi>
2023-08-11 14:26:30 +0300

    Move date picker localization from App.js to DateField and add translation locales

--- src/components/inputs/DateField.js
+++ src/components/inputs/DateField.js
@@ -1,7 +1,11 @@
 import * as React from 'react';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { fiFI, svSE, enUS } from '@mui/x-date-pickers/locales';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import TextField from '@mui/material/TextField';
-import { toISODateStringLocal } from '../../utils/dateUtils';
+import { getDateFnsLocale, toISODateStringLocal } from '../../utils/dateUtils';
+import { useTranslation } from 'react-i18next';
 
 const DateField = ({
   helperText,
@@ -23,6 +27,7 @@ const DateField = ({
       error={error}
       fullWidth={fullWidth}
       variant={variant}
+      placeholder={'testing'}
       sx={{
         '& .MuiInputAdornment-root .MuiButtonBase-root': {
           marginRight: variant === 'standard' ? -1 : -1.5,
@@ -31,18 +36,49 @@ const DateField = ({
     />
   );
 
+  const { t, i18n } = useTranslation();
+  const language = i18n.language;
+
+  const localeTextOverrides = {
+    todayButtonLabel: t('return_to_today'),
+  };
+
   return (
-    <DatePicker
-      disableMaskedInput
-      onChange={(date, keyboardInputValue) => {
-        // Date picker returns Date in user's local time zone
-        // Convert value to ISO date string, ensuring it is always formatted
-        // in the correct time zone
-        onChange && onChange(toISODateStringLocal(date), keyboardInputValue);
-      }}
-      {...props}
-      renderInput={renderInput || defaultRenderInput}
-    />
+    <LocalizationProvider
+      dateAdapter={AdapterDateFns}
+      adapterLocale={getDateFnsLocale(i18n.language)}
+      localeText={
+        {
+          fi: {
+            ...fiFI.components.MuiLocalizationProvider.defaultProps.localeText,
+            ...localeTextOverrides,
+          },
+          sv: {
+            ...svSE.components.MuiLocalizationProvider.defaultProps.localeText,
+            ...localeTextOverrides,
+          },
+          en: {
+            ...enUS.components.MuiLocalizationProvider.defaultProps.localeText,
+            ...localeTextOverrides,
+          },
+          ia: {
+            ...localeTextOverrides,
+          },
+        }[language]
+      }
+    >
+      <DatePicker
+        disableMaskedInput
+        onChange={(date, keyboardInputValue) => {
+          // Date picker returns Date in user's local time zone
+          // Convert value to ISO date string, ensuring it is always formatted
+          // in the correct time zone
+          onChange && onChange(toISODateStringLocal(date), keyboardInputValue);
+        }}
+        {...props}
+        renderInput={renderInput || defaultRenderInput}
+      />
+    </LocalizationProvider>
   );
 };
 
